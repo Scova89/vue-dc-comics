1974e02b22ce954783b54072a3470a016034797c
Francesco <scovascova@gmail.com>
2021-12-13 22:12:58 +0100

    main inferiore

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,7 +4,9 @@
         <p>--Contents goes here--</p>
     </div>
     <div class="main-inf">
-
+        <ul>
+            <li v-for="(link, index) in links" :key="index"><a :href="link.url"><img :src="link.icon" alt="">{{link.text}}</a></li>
+        </ul>
     </div>
 </div>
   
@@ -12,6 +14,40 @@
 
 <script>
 export default {
+    name: 'Main',
+    data () {
+        return {
+            links: [
+                {
+                    icon: "./img/buy-comics-digital-comics.png",
+                    text: "digital comics",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "dc merchandise",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "subscription",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "comic shop locator",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "dc power visa",
+                    url: "#",
+                    current: false,
+                },
+                  
+            ]
+        }
+    }
 
 }
 </script>
@@ -23,6 +59,30 @@ export default {
         p{
             color: white;
             padding: 50px;
+        }
+    }
+
+    .main-inf{
+        background-color: rgb(2 130 249);
+
+        ul{
+            display: flex;
+            list-style: none;
+            justify-content: space-evenly;
+            padding: 50px;
+
+            a{
+                text-decoration: none;
+                color: white;
+                padding-bottom: 30px;
+
+                img{
+                    height: 50px;
+                    object-fit: cover;
+                }
+
+                
+            }
         }
     }
 
